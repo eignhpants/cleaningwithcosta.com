44130c1d1a9264143ad60ef2c7932253cae23f0d
Ian Cullinane <ian.cullinane@gmail.com>
2017-07-11 10:57:38 -0400

    change port to 3010

--- CONFIG.js
+++ CONFIG.js
@@ -1,8 +1,8 @@
 var config = {};
 
-config.title = "Cleaning With Costa"; 
+config.title = "Cleaning With Costa";
 config.name = "Cleaning With Costa";
 config.header = "Ian Cullinane";
-config.port = process.env.DEPLOY_PORT || 3334;
+config.port = process.env.DEPLOY_PORT || 3010;
 
 module.exports = config;
